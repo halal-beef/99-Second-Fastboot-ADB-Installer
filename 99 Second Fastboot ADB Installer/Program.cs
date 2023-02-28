82724563bb15f5e1e7548905c553f5fd43b9a4db
halal-beef <78730004+halal-beef@users.noreply.github.com>
2023-02-28 17:33:15 +0000

    initial and final commit

---
+++ 99 Second Fastboot ADB Installer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO.Compression;
+
+Console.WriteLine("Give me some time we downloading and stuff");
+
+HttpClient client = new();
+
+using (var s = client.GetStreamAsync("https://dl.google.com/android/repository/platform-tools-latest-windows.zip"))
+{
+    using (var fs = new FileStream(Path.GetTempPath() + @"\ptools.zip", FileMode.CreateNew))
+    {
+         s.GetAwaiter().GetResult().CopyTo(fs);
+    }
+}
+
+if (Directory.Exists(@"C:\platform-tools"))
+{
+    Directory.Delete(@"C:\platform-tools", true);
+}
+
+ZipFile.ExtractToDirectory(Path.GetTempPath() + @"\ptools.zip", Path.GetPathRoot(Environment.SystemDirectory), true);
+
+Environment.SetEnvironmentVariable("PATH", Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine) + $@";{Path.GetPathRoot(Environment.SystemDirectory)}platform-tools\\", EnvironmentVariableTarget.Machine);
+
+File.Delete(Path.GetTempPath() + @"\ptools.zip");
